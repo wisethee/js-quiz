f76305afbcbfdc0d161adc0be5b0a4e6945418c6
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-12-04 17:26:50 +0000

    feat: add questions render event listeners

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,6 +2,10 @@
 const startScreenElement = document.getElementById("start-screen");
 const questionsElement = document.getElementById("questions");
 const endScreenElement = document.getElementById("end-screen");
+const feedbackElement = document.getElementById("feedback");
+
+const questionTitle = document.getElementById("question-title");
+const questionChoices = document.getElementById("question-choices");
 
 const timeElement = document.getElementById("time");
 const startElement = document.getElementById("start");
@@ -9,24 +13,111 @@ const startElement = document.getElementById("start");
 /** Inputs */
 let count = 6;
 
-/** Timer */
+let questionsCount = 0;
+const questionsLength = questions.length;
+
+/** Init Screen */
+const init = () => {
+  timeElement.innerHTML = count;
+};
+
+init();
+
+/** Toggle element */
+const renderPageSection = (elementToHide, elementToRender) => {
+  elementToHide.classList.add("hide");
+  elementToRender.classList.remove("hide");
+};
+
+/** End Game */
+const endGame = () => {
+  renderPageSection(questionsElement, endScreenElement);
+};
+
+/** Global Game Timer */
 const startTimer = () => {
   const interval = setInterval(() => {
     count--;
     timeElement.innerHTML = count;
 
     if (count === 0) {
-      startScreenElement.classList.add("hide");
-      endScreenElement.classList.remove("hide");
+      endGame();
       clearInterval(interval);
       return;
     }
   }, 1000);
 };
 
-/** Start */
+/** Destroy question */
+const destroyQuestion = () => {
+  questionTitle.innerHTML = "";
+  questionChoices.innerHTML = "";
+};
+
+/** Hide & Show Feedback element */
+const hideFeedback = () => {
+  feedbackElement.classList.add("hide");
+};
+
+const showFeedback = () => {
+  feedbackElement.classList.remove("hide");
+};
+
+/** Check Answer */
+const checkAnswer = (id, answer) => {
+  showFeedback();
+  console.log(feedbackElement);
+  if (answer === Number(id)) {
+    feedbackElement.innerHTML = "correct";
+  } else {
+    feedbackElement.innerHTML = "wrong";
+  }
+};
+
+/** Build Question Element */
+const buildQuestion = (question) => {
+  const { title, answer, choices } = question;
+
+  questionTitle.innerHTML = title;
+
+  choices.forEach((choice, index) => {
+    const questionElement = document.createElement("button");
+    questionElement.setAttribute("id", index);
+    questionElement.innerHTML = choice;
+    questionElement.addEventListener("click", (event) => {
+      const { id } = event.target;
+
+      questionsCount++;
+
+      checkAnswer(id, answer);
+
+      const questionTimeout = setTimeout(() => {
+        if (questionsCount !== questionsLength) {
+          destroyQuestion();
+          buildQuestion(questions[questionsCount]);
+        } else {
+          renderPageSection(questionsElement, endScreenElement);
+          hideFeedback();
+        }
+      }, 300);
+    });
+
+    hideFeedback();
+
+    questionChoices.appendChild(questionElement);
+  });
+};
+
+/** Render Questions */
+const renderQuestions = () => {
+  renderPageSection(startScreenElement, questionsElement);
+  buildQuestion(questions[questionsCount]);
+};
+
+/** Start Quiz */
 const startHandler = () => {
   startTimer();
+  renderQuestions();
 };
 
 /** Event Listeners */
---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    title: "Question 1",
+    choices: [
+      "First answer 1",
+      "Second answer 1",
+      "Third answer 1",
+      "Forth answer 1",
+    ],
+    answer: 3,
+  },
+  {
+    title: "Question 2",
+    choices: [
+      "First answer 2",
+      "Second answer 2",
+      "Third answer 2",
+      "Forth answer 2",
+    ],
+    answer: 1,
+  },
+  {
+    title: "Question 3",
+    choices: [
+      "First answer 3",
+      "Second answer 3",
+      "Third answer 3",
+      "Forth answer 3",
+    ],
+    answer: 4,
+  },
+];
